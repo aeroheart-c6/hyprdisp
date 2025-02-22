2d2777a9cd9a1a08202712dae3e53a5921a8d5ad
aeroheart-c6 <aeroheart.c6@gmail.com>
2025-02-23 00:17:30 +0800

    parse hyprland monitors output into Monitor structs

--- hyprdisp/hypr/cmd.go
+++ hyprdisp/hypr/cmd.go
@@ -1,26 +1,52 @@
 package hypr
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"regexp"
 	"strings"
 	"time"
 )
 
 type Monitor struct {
-	Num    int
-	ID     string
-	Make   string
-	Model  string
-	Serial string
+	ID          string
+	Name        string
+	Resolution  string
+	Position    string
+	Description string
+	Make        string
+	Model       string
+	Serial      string
 }
 
-func (m Monitor) Description() string {
-	return fmt.Sprintf("%v %v %v",
-		m.Make,
-		m.Model,
-		m.Serial,
-	)
+func (m *Monitor) set(field string, value string) error {
+	switch field {
+	case "id":
+		m.ID = value
+	case "name":
+		m.Name = value
+	case "resolution":
+		m.Resolution = value
+	case "position":
+		m.Position = value
+	case "description":
+		m.Description = value
+	case "make":
+		m.Make = value
+	case "model":
+		m.Model = value
+	case "serial":
+		m.Serial = value
+	default:
+		return fmt.Errorf("unsupported field: %v with value %v", field, value)
+	}
+
+	return nil
+}
+
+func (m Monitor) Empty() bool {
+	return m.Name == ""
 }
 
 func GetMonitors() {
@@ -46,30 +72,107 @@ func GetMonitors() {
 	socketConn.Write([]byte("monitors"))
 
 	var (
-		dataLength int    = 512
-		dataCount  int    = 0
-		data       []byte = make([]byte, dataLength)
-		body       strings.Builder
+		bufferSize int    = 512
+		buffer     []byte = make([]byte, bufferSize)
+		dataSize   int    = 0
+		data       strings.Builder
 	)
 
 	for {
 		socketConn.SetReadDeadline(time.Now().Add(1 * time.Second))
-		dataCount, err = socketConn.Read(data)
+		dataSize, err = socketConn.Read(buffer)
 		if err != nil {
 			fmt.Printf("%v", err)
 			break
 		}
 
-		body.Write(data[:dataCount])
+		data.Write(buffer[:dataSize])
 
-		if dataCount < dataLength {
+		if dataSize < bufferSize {
 			break
 		}
 	}
 
-	fmt.Printf("MONITORS\n%v\n", string(body.String()))
+	fmt.Printf("MONITORS\n%v\n", data.String())
 }
 
-func parseMonitorsBody(body string) []Monitor {
-	return nil
+var (
+	regexMonitorID  *regexp.Regexp = regexp.MustCompile(`Monitor (?P<id>[\w-]+) \(ID (?P<no>\d+)\)`)
+	regexMonitorRes *regexp.Regexp = regexp.MustCompile(`` +
+		`(?P<resolution>\d+x\d+@\d+\.\d+)` +
+		` at ` +
+		`(?P<position>\d+x\d+)`,
+	)
+)
+
+func parseMonitorsPayload(body string) ([]Monitor, error) {
+	var (
+		lines        []string  = strings.Split(body, "\n")
+		monitors     []Monitor = make([]Monitor, 0, 5)
+		bodyStartIdx int       = 0
+		bodyEndIdx   int       = len(lines)
+	)
+
+	for {
+		if bodyStartIdx > bodyEndIdx {
+			break
+		}
+
+		var (
+			monitor Monitor
+			lineIdx int
+			line    string
+		)
+		for lineIdx, line = range lines[bodyStartIdx:bodyEndIdx] {
+			line = strings.TrimSpace(line)
+
+			if len(line) == 0 {
+				// Terminating line
+				if !monitor.Empty() {
+					monitors = append(monitors, monitor)
+				}
+				break
+			} else if lineIdx == 0 {
+				// Monitor IDs
+				var matches []string = regexMonitorID.FindStringSubmatch(line)
+				if matches == nil {
+					return nil, errors.New("invalid monitor payload")
+				}
+
+				monitor = Monitor{}
+				monitor.set("id", matches[2])
+				monitor.set("name", matches[1])
+			} else if lineIdx == 1 {
+				// Monitor Resolution
+				var matches []string = regexMonitorRes.FindStringSubmatch(line)
+				if matches == nil {
+					return nil, errors.New("invalid monitor resolution spec")
+				}
+
+				monitor.set("resolution", matches[1])
+				monitor.set("position", matches[2])
+			} else {
+				var (
+					field string
+					value string
+					found bool
+					err   error
+				)
+
+				field, value, found = strings.Cut(line, ":")
+				if !found {
+					return nil, errors.New("uanble to parse a field / value pair")
+				}
+
+				err = monitor.set(field, strings.TrimSpace(value))
+				if err != nil {
+					fmt.Printf("whoops: %v\n", err)
+				}
+			}
+		}
+
+		bodyStartIdx = bodyStartIdx + lineIdx + 1
+	}
+
+	return monitors, nil
 }
---
+++ hyprdisp/hypr/cmd_test.go
@@ -0,0 +1,63 @@
+package hypr
+
+import (
+	"io"
+	"os"
+	"testing"
+)
+
+func Test_parseMonitorsPayload(t *testing.T) {
+	var (
+		file    *os.File
+		data    []byte
+		monitor Monitor
+		err     error
+	)
+
+	file, err = os.Open("testdata/monitors.txt")
+	if err != nil {
+		t.Fatalf("failed reading sample monitors.txt file: %v", err)
+	}
+
+	defer func() {
+		var err error = file.Close()
+		if err != nil {
+			t.Fatalf("failed closing the file")
+		}
+	}()
+
+	data, err = io.ReadAll(file)
+	if err != nil {
+		t.Fatalf("failed to read the file: %v", err)
+	}
+
+	monitors, err := parseMonitorsPayload(string(data))
+	if err != nil {
+		t.Fatalf("why am I returning an error???: %v", err)
+	}
+
+	if len(monitors) < 3 {
+		t.Fatalf("expected 3 monitors to be extracted")
+	}
+
+	monitor = monitors[0]
+	if monitor.ID != "0" ||
+		monitor.Name != "DP-1" ||
+		monitor.Resolution != "3440x1440@120.00000" ||
+		monitor.Position != "0x1080" {
+		t.Fatalf("monitor 0 has invalid values")
+	}
+
+	if monitor.Description != "Test Description 1" {
+		t.Fatalf("monitor 0 has invalid description")
+	}
+	if monitor.Make != "Test Make 1" {
+		t.Fatalf("monitor 0 has invalid make")
+	}
+	if monitor.Model != "Test Ultrawide Monitor" {
+		t.Fatalf("monitor 0 has invalid model")
+	}
+	if monitor.Serial != "" {
+		t.Fatalf("monitor 0 has invalid description")
+	}
+}
