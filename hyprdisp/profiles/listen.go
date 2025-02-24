a303ee653fa019adc188ef1935e25f079fbefc6f
aeroheart-c6 <aeroheart.c6@gmail.com>
2025-02-24 22:42:00 +0800

    add timer to trigger after receiving the events

--- hyprdisp/profiles/listen.go
+++ hyprdisp/profiles/listen.go
@@ -3,10 +3,21 @@ package profiles
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"aeroheart.io/hyprdisp/hypr"
 )
 
+// TODO it's actually just 2 - 3 seconds wait
+const triggerDuration time.Duration = 5 * time.Second
+
+var (
+	timer        *time.Timer
+)
+
+func Init(ctx context.Context) {
+	timer = time.NewTimer(0)
+}
 
 func ListenEvents(ctx context.Context, errs chan error, events chan hypr.Event) {
 	var logger *log.Logger = ctx.Value(sys.ContextKeyLogger).(*log.Logger)
@@ -18,9 +29,19 @@ func ListenEvents(ctx context.Context, errs chan error, events chan hypr.Event) 
 		}
 
 		fmt.Printf("Got monitor event: %v\n", event)
+		timer.Reset(triggerDuration)
 	}
 }
 
+func ListenTimer(ctx context.Context, errs chan error) {
+	var logger *log.Logger = ctx.Value(sys.ContextKeyLogger).(*log.Logger)
+
+	for moment := range timer.C {
+
+		triggerConfigUpdates()
 	}
+}
+
+func triggerConfigUpdates() {
 
 }
